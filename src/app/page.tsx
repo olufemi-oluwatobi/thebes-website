6e9b991dfe00c00ad33510905b44e6d1b36c9360
Oluwatobi Olufemi <olufemioluwatobi1996@gmail.com>
2024-04-22 16:59:09 +0100

    add padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ import FeatureContestBanner from "@/components/FeatureContestBanner";
 
 export default function Home() {
   return (
-    <main className="flex bg-white w-screen font-sans min-h-screen flex-col ">
+    <main className="flex bg-white w-screen text-brown font-sans min-h-screen flex-col ">
       <HeroSection />
       <AboutSection />
       <HowItWorksSection />
       <Calendar />
       <FAQSection />
       <div className="relative w-full mt-8">
-        <div className="w-full flex justify-center bg-black bg-opacity-0 -mb-[35vh] z-20 px-[5%] md:px-0">
+        <div className="w-full flex justify-center bg-black bg-opacity-0 -mb-[50vh] md:-mb-[35vh] z-20 px-[5%] md:px-0">
           <FeatureContestBanner />
         </div>
 
