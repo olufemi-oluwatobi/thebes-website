c3b0a998a136bd3ba982058fbf88ce50936a49f8
Oluwatobi Olufemi <olufemioluwatobi1996@gmail.com>
2024-04-25 13:28:17 +0100

    content

--- src/components/ui/Accordion/index.tsx
+++ src/components/ui/Accordion/index.tsx
@@ -22,13 +22,17 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, content }) => {
           isOpen && " border-l-4 border-purple"
         } justify-between items-center font-semibold p-6 cursor-pointer`}
       >
-        <span className="  font-bold text-xl font-sans ">{title}</span>
+        <span className=" max-w-[80%]  font-bold text-md font-sans ">
+          {title}
+        </span>
         <svg
           className={`fill-current text-purple h-6 w-6 transform transition-transform duration-500 ${
             isOpen ? "rotate-180" : ""
           }`}
           stroke="#836CBB"
           strokeWidth={2}
+          width={20}
+          height={20}
           viewBox="0 0 20 20"
         >
           <path
@@ -43,9 +47,9 @@ const AccordionItem: React.FC<AccordionItemProps> = ({ title, content }) => {
         style={{
           maxHeight: isOpen ? `${contentRef.current?.scrollHeight}px` : "0",
         }}
-        className="border-l-4 border-purple w-full overflow-hidden transition-max-height duration-500"
+        className="border-l-4 border-purple  w-full overflow-hidden transition-max-height duration-500"
       >
-        <p className="px-6 pb-6 text-gray-900">{content}</p>
+        <p className="px-6 pb-6 text-gray-200">{content}</p>
       </div>
     </li>
   );
